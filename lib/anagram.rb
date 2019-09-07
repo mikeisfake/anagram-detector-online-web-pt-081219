97603932c23c94399f2bee6547990142868ef663
Mike Cooper <mikescooper@icloud.com>
2019-09-07 21:46:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(match_array) 
+    
+    split_word = @word.split 
+    
+  end 
+  
+  
+end 
