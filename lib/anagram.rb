55f7125c3f5ae1816931c823d734dbdd34938ea2
Mike Cooper <mikescooper@icloud.com>
2019-09-07 22:02:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(match_array) 
+    
+    split_word = @word.split(//).sort 
+    split_match_array = match_array.map { |i| i.split(//).sort }
+    
+    matches = []
+    
+    split_match_array.each_with_index { |letters , i| letters == split_word ? matches << match_array[i] : nil }
+    matches
+    
+  end 
+  
+  
+end 
